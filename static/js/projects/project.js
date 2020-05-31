0905ffb9a1fba39ed58eadb06228912dbdaec006
Jitzek <Jitzek@users.noreply.github.com>
2020-05-31 17:56:48 +0200

    test

--- static/js/projects/project.js
+++ static/js/projects/project.js
@@ -60,3 +60,85 @@ function tagsAsHTML(tags) {
     return output + '</div>';
 }
 
+class Project {
+    title = 'undefined';
+    desc = 'undefined';
+    img = 'undefined';
+    pr_lang = 'undefined';
+    teamsize = 0;
+    tags = [];
+
+    constructor(title, desc, img, pr_lang, teamsize, tags = []) {
+        this.title = title;
+        this.desc = desc;
+        this.img = img;
+        this.pr_lang = pr_lang;
+        this.teamsize = teamsize;
+        this.tags = tags;
+    }
+
+    get includesHTML() {
+        return `
+            <link rel="stylesheet" href="/static/css/projects/project.css" />
+        `;
+    }
+
+    get headHTML() {
+        return `
+        <!-- JavaScript Generated -->
+        <!-- Image and Title -->
+        <div style="display: flex;">
+            <div style="display: flex; max-height: 4.5em;">
+                <img style="width: 80px; height: auto; margin-right: 0.25em;" src="${this.img}"
+                    class="img-fluid" />
+                <div>
+                    <h2 style="color: white;">${this.title}</h2>
+                    <!-- Tags -->
+                    <div style="min-height: 2em; max-height: 2em; overflow: auto; line-height: 2em;">
+                        ${tagsAsHTML(this.tags)}
+                    </div>
+                </div>
+            </div>
+            <!--<div style="position: absolute; margin: 45px 0px 0px 80px;">
+                ${tagsAsHTML(this.tags)}
+            </div>-->
+        </div><br>
+        <!-- Description -->
+        <div style="margin-top: 1em;">
+            <p>
+                ${this.desc}
+            </p>
+        </div>
+        <table style="width: 100%; font-size: 1em;">
+            <tbody>
+                <tr>
+                    <td style="min-width: 5em; height: 2em; padding: 0.4em;">
+                        <img style="width: 2.75em; height: auto; background-color: black; padding: 0.4em;"
+                            src="/static/src/icons/github_logo.png" class="img-fluid" alt="Language" /><span style="padding: 0.4em; background-color: ${GITHUB_COLOR};">
+                            <a href="https://github.com/Jitzek/isala-web-app" class="href-link gh-repo" target="_blank">https://github.com/Jitzek/isala-web-app</a>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="min-width: 5em; height: 2em; padding: 0.4em;">
+                        <img style="width: 2.75em; height: auto; background-color: black; padding: 0.4em;"
+                            src="/static/src/icons/programming_lang_white.png" class="img-fluid" alt="Language" /><span style="padding: 0.4em; background-color: ${getColorOfLanguage(this.pr_lang)};">
+                            <img style="width: 1.5em; height: auto;"
+                                src="${getLogoOfLanguage(this.pr_lang)}" class="img-fluid" /><span
+                                style="color: white; margin-left: 0.5em;">${this.pr_lang}</span>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="min-width: 5em; height: 2em; padding: 0.4em;">
+                        <img style="width: 2.75em; height: auto; background-color: black; padding: 5px;"
+                            src="/static/src/icons/team_icon.png" class="img-fluid" alt="Language" /><span style="padding: 5px; background-color: ${TEAMSIZE_COLOR};">
+                            <span style="color: white;">${this.teamsize}</span>
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        `;
+    }
+}
