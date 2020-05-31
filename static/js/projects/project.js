1a3c59a835339d89bab4126318251590ee82a948
Jitzek <Jitzek@users.noreply.github.com>
2020-05-31 17:45:49 +0200

    test

--- static/js/projects/project.js
+++ static/js/projects/project.js
@@ -69,7 +69,6 @@ class Project {
     tags = [];
 
     constructor(title, desc, img, pr_lang, teamsize, tags = []) {
-        document.write('constructor');
         this.title = title;
         this.desc = desc;
         this.img = img;
