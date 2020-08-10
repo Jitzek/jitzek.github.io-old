39b7eff8b77733806c8d142e16ea496ffb59197d
Jitzek <Jitz_k@hotmail.com>
2020-08-10 18:40:37 +0200

    json restructure, getLocation addition

--- static/js/linux/filesystem.js
+++ static/js/linux/filesystem.js
@@ -3,40 +3,56 @@ var Filesystem = /** @class */ (function () {
         this.storage = {
             name: "/",
             type: "directory",
-            content: {
-                bin: {
+            content: [
+                {
                     name: "bin",
                     type: "directory",
-                    content: {
-                        "test.txt": {
+                    content: [
+                        {
                             name: "test.txt",
                             type: "file",
-                            content: "this is a file"
-                        }
-                    }
+                            executable: false,
+                            content: "this is a text file"
+                        },
+                    ]
                 },
-                boot: {
+                {
                     name: "boot",
                     type: "directory",
-                    content: {}
-                }
-            }
+                    content: []
+                },
+            ]
         };
         this.current_dir = this.storage;
     }
     Filesystem.prototype.getLocation = function (path) {
         // Convert string to array (cutting out '/')
         var path_arr = this.convertToLegalPathArray(this.pathAsArray(path));
-        console.log(path_arr);
-        while (true) {
-            break;
+        console.log(this.getLocationFromArray(path_arr));
+        return this.getLocationFromArray(path_arr);
+    };
+    Filesystem.prototype.getLocationFromArray = function (array, current_location) {
+        if (current_location === void 0) { current_location = this.storage; }
+        if (array.length < 1)
+            return current_location;
+        if (array[0] == '/')
+            array.shift();
+        var location = null;
+        for (var i = 0; i < current_location.content.length; i++) {
+            if (current_location.content[i].name == array[0]) {
+                location = current_location.content[i];
+                break;
+            }
         }
+        if (location == null || location == undefined)
+            return false;
+        return this.getLocationFromArray(array.slice(1), location);
     };
     Filesystem.prototype.pathAsArray = function (path) {
         var path_arr = [];
         var pointer = 0;
         for (var i = 0; i < path.length; i++) {
-            if (path[i] == '/') {
+            if (path[i] == "/") {
                 if (path_arr[pointer] == undefined)
                     path_arr.push("/");
                 pointer++;
@@ -50,18 +66,20 @@ var Filesystem = /** @class */ (function () {
         return path_arr;
     };
     Filesystem.prototype.convertToLegalPathArray = function (path) {
-        if (path[0][0] == '.') {
-            var pre_path = path[0] == '..' ? this.locationAsArray(this.getParentOfLocation(this.current_dir)) : this.locationAsArray(this.current_dir);
+        if (path[0][0] == ".") {
+            var pre_path = path[0] == ".."
+                ? this.locationAsArray(this.getParentOfLocation(this.current_dir))
+                : this.locationAsArray(this.current_dir);
             path = pre_path.concat(path.slice(1));
         }
         for (var i = 0; i < path.length; i++) {
-            if (path[i] == '.')
+            if (path[i] == ".")
                 path.splice(i, 1);
-            else if (path[i] == '..')
+            else if (path[i] == "..")
                 path.splice(i, -2);
         }
-        if (path[0] != '/')
-            path.unshift('/');
+        if (path[0] != "/")
+            path.unshift("/");
         return path;
     };
     Filesystem.prototype.locationAsArray = function (location) {
@@ -78,18 +96,16 @@ var Filesystem = /** @class */ (function () {
         return result;
     };
     Filesystem.prototype.getParentOfLocation = function (location, current_location) {
-        var _this = this;
         if (current_location === void 0) { current_location = this.storage; }
-        if (location = this.storage)
+        if (location == this.storage)
             return this.storage;
-        Object.keys(current_location.content).forEach(function (key) {
-            if (current_location.content[key] == location) {
+        for (var i = 0; i < current_location.content.length; i++) {
+            if (current_location.content[i] == location)
                 return current_location;
-            }
-            var result = _this.getParentOfLocation(location, current_location);
+            var result = this.getParentOfLocation(location, current_location.content[i]);
             if (result !== false)
                 return result;
-        });
+        }
         return false;
     };
     return Filesystem;
--- static/js/linux/filesystem.ts
+++ static/js/linux/filesystem.ts
@@ -5,63 +5,78 @@ class Filesystem {
     this.storage = {
       name: "/",
       type: "directory",
-      content: {
-        bin: {
+      content: [
+        {
           name: "bin",
           type: "directory",
-          content: {
-            "test.txt": {
+          content: [
+            {
               name: "test.txt",
               type: "file",
-              content: "this is a file",
+              executable: false,
+              content: "this is a text file",
             },
-          },
+          ],
         },
-        boot: {
-            name: "boot",
-            type: "directory",
-            content: {}
-        }
-      },
+        {
+          name: "boot",
+          type: "directory",
+          content: [],
+        },
+      ],
     };
     this.current_dir = this.storage;
   }
 
-  getLocation(path: string) {
+  getLocation(path: string): any {
     // Convert string to array (cutting out '/')
     let path_arr = this.convertToLegalPathArray(this.pathAsArray(path));
-    console.log(path_arr);
+    
+    return this.getLocationFromArray(path_arr);
+  }
 
-    while(true) {
-      break;
+  getLocationFromArray(array: Array<string>, current_location = this.storage): any {
+    if (array.length < 1) return current_location;
+    if (array[0] == '/') array.shift();
+    let location: Object = null;
+    for (let i = 0; i < current_location.content.length; i++) {
+      if (current_location.content[i].name == array[0]) {
+        location = current_location.content[i];
+        break;
+      }
     }
+    if (location == null || location == undefined) return false;
+    return this.getLocationFromArray(array.slice(1), location);
   }
 
   private pathAsArray(path: string): Array<string> {
     let path_arr: Array<string> = [];
     let pointer = 0;
     for (let i = 0; i < path.length; i++) {
-      if (path[i] == '/') {
+      if (path[i] == "/") {
         if (path_arr[pointer] == undefined) path_arr.push("/");
         pointer++;
         continue;
       }
-      if (path_arr[pointer] == undefined) path_arr.push(path[i])
+      if (path_arr[pointer] == undefined) path_arr.push(path[i]);
       else path_arr[pointer] += path[i];
     }
     return path_arr;
   }
 
   private convertToLegalPathArray(path: Array<string>): Array<string> {
-    if (path[0][0] == '.') {
-      let pre_path: Array<string> = path[0] == '..' ? this.locationAsArray(this.getParentOfLocation(this.current_dir)) : this.locationAsArray(this.current_dir);
+    if (path[0][0] == ".") {
+      let pre_path: Array<string> =
+        path[0] == ".."
+          ? this.locationAsArray(this.getParentOfLocation(this.current_dir))
+          : this.locationAsArray(this.current_dir);
       path = pre_path.concat(path.slice(1));
     }
     for (let i = 0; i < path.length; i++) {
-      if (path[i] == '.') path.splice(i, 1);
-      else if (path[i] == '..') path.splice(i, -2);
+      if (path[i] == ".") path.splice(i, 1);
+      else if (path[i] == "..") path.splice(i, -2);
     }
-    if (path[0] != '/') path.unshift('/');
+    if (path[0] != "/") path.unshift("/");
     return path;
   }
 
@@ -77,15 +92,19 @@ class Filesystem {
     return result;
   }
 
-  private getParentOfLocation(location: Object, current_location: any = this.storage): any {
-    if (location = this.storage) return this.storage;
-    Object.keys(current_location.content).forEach(key => {
-      if (current_location.content[key] == location) {
-        return current_location;
-      }
-      let result = this.getParentOfLocation(location, current_location);
+  private getParentOfLocation(
+    location: Object,
+    current_location: any = this.storage
+  ): any {
+    if (location == this.storage) return this.storage;
+    for (let i = 0; i < current_location.content.length; i++) {
+      if (current_location.content[i] == location) return current_location;
+      let result = this.getParentOfLocation(
+        location,
+        current_location.content[i]
+      );
       if (result !== false) return result;
-    });
+    }
     return false;
   }
 }
